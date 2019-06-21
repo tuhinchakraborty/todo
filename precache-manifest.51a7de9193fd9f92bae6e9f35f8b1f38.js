b7b2385113357b21875830a0189ed914f0ca77d3
Tuhin Chakraborty <tuhin_chakraborty@outlook.com>
2019-06-21 20:30:11 +0530

    Updates

---
+++ precache-manifest.51a7de9193fd9f92bae6e9f35f8b1f38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "015654431640d1c5189e",
+    "url": "/todo/static/css/main.35a754c7.chunk.css"
+  },
+  {
+    "revision": "015654431640d1c5189e",
+    "url": "/todo/static/js/main.2d8afa8f.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "8dc247f1a0fc5e898543",
+    "url": "/todo/static/js/2.455627ce.chunk.js"
+  },
+  {
+    "revision": "0b4b3f0a620145a079d6e3164ac4418d",
+    "url": "/todo/index.html"
+  }
+];
